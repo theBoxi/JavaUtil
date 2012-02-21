d979b8432ee9e78ef31969a5c88417a14e8ef8f0
boxi <boxi@Linde>
2012-02-21 22:44:07 +0100

    Javadoc added

--- JavaUtil/src/ch/boxi/javaUtil/id/checkDigit/CheckDigitAlgorythm.java
+++ JavaUtil/src/ch/boxi/javaUtil/id/checkDigit/CheckDigitAlgorythm.java
@@ -1,8 +1,31 @@
 package ch.boxi.javaUtil.id.checkDigit;
 
 public interface CheckDigitAlgorythm {
+	/**
+	 * Split number from ID-WithCheckDigits
+	 * @param id
+	 * @returs number without the checkdigits	
+	 */
 	public long getNumber(long id);
+	
+	/**
+	 * Split checkDigits from id
+	 * @param id
+	 * @return checkdigits
+	 */
 	public long extractCheckDigit(long id);
+	
+	/**
+	 * Checks if this ID-With-CheckDigis is Valid. Split number an checkdigits and test it.
+	 * @param id
+	 * @return true if valid, otherwise false ;-)
+	 */
 	public boolean isValidID(long id);
+	
+	/**
+	 * Add Checkdigits to the ID
+	 * @param id ID-without Checkdigits
+	 * @return ID-With Checkdigits
+	 */
 	public long AddCheckDigit(long id);
 }
